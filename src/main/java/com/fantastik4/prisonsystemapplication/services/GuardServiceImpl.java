6da4429bc1f564ab4757170d322770cb3bf939a2
Juraj Sabik <jsb@trifork.com>
2022-05-12 14:09:23 +0200

    removed guard static password

--- src/main/java/com/fantastik4/prisonsystemapplication/services/GuardServiceImpl.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/services/GuardServiceImpl.java
@@ -28,6 +28,7 @@ public class GuardServiceImpl implements GuardService{
     public String createGuard(Guard newGuard) {
         try {
 //            newGuard.setPassword("123456");
+
             Guard g = restTemplate.postForObject("https://localhost:7150/Guard", newGuard, Guard.class);
             return gson.toJson(g);
 //            return "success";
