3f0a576a239d85229c6e63b622aa9c43d4037ea4
Benjamín Bartošík <benjamin.bartosik@gmail.com>
2022-05-12 11:57:14 +0200

    prisoners checked & enhanced

--- src/main/java/com/fantastik4/prisonsystemapplication/services/PrisonerService.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/services/PrisonerService.java
@@ -3,14 +3,13 @@ package com.fantastik4.prisonsystemapplication.services;
 import com.fantastik4.prisonsystemapplication.model.Prisoner;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public interface PrisonerService {
-    String addPrisoner(String newPrisoner);
+    String createPrisoner(String newPrisoner);
     String removePrisoner(Long id);
-    Prisoner getPrisonerById(Long prisonerId);
-    List<Prisoner> getPrisoners();
+    String getPrisonerById(Long prisonerId);
+    String getPrisoners();
     String updatePrisoner(String jsonPrisoner);
 }
