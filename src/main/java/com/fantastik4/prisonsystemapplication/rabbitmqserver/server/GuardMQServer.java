ee57621c9cc2a617836be1359cdf5bdb7697dddf
Benjamín Bartošík <benjamin.bartosik@gmail.com>
2022-05-13 09:20:26 +0200

    working on EmailService

--- src/main/java/com/fantastik4/prisonsystemapplication/rabbitmqserver/server/GuardMQServer.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/rabbitmqserver/server/GuardMQServer.java
@@ -8,8 +8,6 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class GuardMQServer {
     private GuardService guardService;
--- src/main/java/com/fantastik4/prisonsystemapplication/services/config/RestTemplateConfig.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/services/config/RestTemplateConfig.java
@@ -7,6 +7,9 @@ import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
@@ -14,6 +17,7 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 @Configuration
 public class RestTemplateConfig{
@@ -38,5 +42,30 @@ public class RestTemplateConfig{
         requestFactory.setHttpClient(httpClient);
         RestTemplate restTemplate = new RestTemplate(requestFactory);
         return restTemplate;
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    @Bean
+    public SimpleMailMessage templateSimpleMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText("This is the test email template for your email:\n%s\n");
+        return message;
     }
 }
