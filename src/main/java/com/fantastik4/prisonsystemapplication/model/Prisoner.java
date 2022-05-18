52f726c8aa45151a725eac339924f145275ef5a6
Benjamín Bartošík <benjamin.bartosik@gmail.com>
2022-05-18 14:29:48 +0200

    prisoner's attributes finished

--- src/main/java/com/fantastik4/prisonsystemapplication/model/Prisoner.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/model/Prisoner.java
@@ -17,14 +17,15 @@ public class Prisoner {
     public String crimeCommitted;
     public int points;
     public String note;
-    public LocalDateTime entryDate;
+//    public LocalDateTime entryDate;
+    public String entryDate;
     public int durationInMonths;
-
+    public Sector sector;
 
     public Prisoner() {
     }
 
-    public Prisoner(long id, String firstName, String lastName, int ssn, String crimeCommitted, int points, String note, LocalDateTime entryDate, int durationInMonths) {
+    public Prisoner(long id, String firstName, String lastName, int ssn, String crimeCommitted, int points, String note, String entryDate, int durationInMonths, Sector sector) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -34,6 +35,7 @@ public class Prisoner {
         this.note = note;
         this.entryDate = entryDate;
         this.durationInMonths = durationInMonths;
+        this.sector = sector;
     }
 
     public long getId() {
@@ -44,69 +46,6 @@ public class Prisoner {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.ssn = ssn;
-    }
-
-    public String getCrimeCommitted() {
-        return crimeCommitted;
-    }
-
-    public void setCrimeCommitted(String crimeCommitted) {
-        this.crimeCommitted = crimeCommitted;
-    }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public LocalDateTime getEntryDate() {
-        return entryDate;
-    }
-
-    public void setEntryDate(LocalDateTime entryDate) {
-        this.entryDate = entryDate;
-    }
-
-    public int getDurationInMonths() {
-        return durationInMonths;
-    }
-
-    public void setDurationInMonths(int durationInMonths) {
-        this.durationInMonths = durationInMonths;
-    }
 
     @Override
     public String toString() {
