dffd22676d629f869bd8234a9f1d9734cbb7ef98
Mike Abbey <mihaiavram72@gmail.com>
2022-06-01 12:58:20 +0200

    fixed changing password

--- src/main/java/com/fantastik4/prisonsystemapplication/rabbitmqservers/configs/RabbitMQGuardConfig.java
+++ src/main/java/com/fantastik4/prisonsystemapplication/rabbitmqservers/configs/RabbitMQGuardConfig.java
@@ -10,109 +10,117 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQGuardConfig {
     @Bean
-    public DirectExchange guardExchange(){
+    public DirectExchange guardExchange() {
         return new DirectExchange("guard.exchange");
     }
 
     @Bean
-    public Queue guardQueue(){
+    public Queue guardQueue() {
         return new Queue("guard.add");
     }
+
     @Bean
-    public Binding guardBinding(){
+    public Binding guardBinding() {
         return BindingBuilder.bind(guardQueue()).to(guardExchange()).with("guard.add");
     }
 
     @Bean
-    public Queue guardRemoveQueue(){
+    public Queue guardRemoveQueue() {
         return new Queue("guard.remove");
     }
+
     @Bean
-    public Binding guardRemoveBinding(){
+    public Binding guardRemoveBinding() {
         return BindingBuilder.bind(guardRemoveQueue()).to(guardExchange()).with("guard.remove");
     }
 
     @Bean
-    public Queue guardsGetQueue(){
+    public Queue guardsGetQueue() {
         return new Queue("guards.get");
     }
+
     @Bean
-    public Binding guardsGetBinding(){
+    public Binding guardsGetBinding() {
         return BindingBuilder.bind(guardsGetQueue()).to(guardExchange()).with("guards.get");
     }
 
     @Bean
-    public Queue guardGetByIdQueue(){
+    public Queue guardGetByIdQueue() {
         return new Queue("guard.getById");
     }
+
     @Bean
-    public Binding guardGetByIdBinding(){
+    public Binding guardGetByIdBinding() {
         return BindingBuilder.bind(guardGetByIdQueue()).to(guardExchange()).with("guard.getById");
     }
 
     @Bean
-    public Queue guardUpdateQueue(){
+    public Queue guardUpdateQueue() {
         return new Queue("guard.update");
-    }
-    @Bean
-    public Binding guardUpdateBinding(){
-        return BindingBuilder.bind(guardUpdateQueue()).to(guardExchange()).with("guard.update");
-    }
-    @Bean
-    public Queue guardSectorQueue(){
-        return new Queue("guard.getBySector");
-    }
-    @Bean
-    public Binding guardSectorBinding(){
-        return BindingBuilder.bind(guardSectorQueue()).to(guardExchange()).with("guard.getBySector");
-    }
-    @Bean
-    public Queue getGuardsPerSectTodayQueue(){
-        return new Queue("guard.getPerSectorToday");
-    }
-    @Bean
-    public Binding getGuardsPerSectTodayBinding(){
-        return BindingBuilder.bind(getGuardsPerSectTodayQueue()).to(guardExchange()).with("guard.getPerSectorToday");
-    }
-    @Bean
-    public Queue getNumGuardsPerSectQueue(){
-        return new Queue("guard.getNumPerSector");
-    }
-    @Bean
-    public Binding getNumGuardsPerSectBinding(){
-        return BindingBuilder.bind(getNumGuardsPerSectQueue()).to(guardExchange()).with("guard.getNumPerSector");
-    }
-    @Bean
-    public Queue isGuardAssignedQueue(){
-        return new Queue("guard.isAssigned");
-    }
-    @Bean
-    public Binding isGuardAssignedBinding(){
-        return BindingBuilder.bind(isGuardAssignedQueue()).to(guardExchange()).with("guard.isAssigned");
-    }
-    @Bean
-    public Queue getNumGuardsPerSectTodayQueue(){
-        return new Queue("guard.getNumPerSectorToday");
-    }
-    @Bean
-    public Binding getNumGuardsPerSectTodayBinding(){
-        return BindingBuilder.bind(getNumGuardsPerSectTodayQueue()).to(guardExchange()).with("guard.getNumPerSectorToday");
-    }
-    @Bean
-    public Queue isGuardWorkingQueue(){
-        return new Queue("guard.isWorking");
-    }
-    @Bean
-    public Binding isGuardWorkingBinding(){
-        return BindingBuilder.bind(isGuardWorkingQueue()).to(guardExchange()).with("guard.isWorking");
     }
 
     @Bean
-    public Queue changePasswordQueue(){
-        return new Queue("guard.changePassword");
+    public Binding guardUpdateBinding() {
+        return BindingBuilder.bind(guardUpdateQueue()).to(guardExchange()).with("guard.update");
     }
+
     @Bean
-    public Binding changePasswordBinding(){
-        return BindingBuilder.bind(changePasswordQueue()).to(guardExchange()).with("guard.changePassword");
+    public Queue guardSectorQueue() {
+        return new Queue("guard.getBySector");
+    }
+
+    @Bean
+    public Binding guardSectorBinding() {
+        return BindingBuilder.bind(guardSectorQueue()).to(guardExchange()).with("guard.getBySector");
+    }
+
+    @Bean
+    public Queue getGuardsPerSectTodayQueue() {
+        return new Queue("guard.getPerSectorToday");
+    }
+
+    @Bean
+    public Binding getGuardsPerSectTodayBinding() {
+        return BindingBuilder.bind(getGuardsPerSectTodayQueue()).to(guardExchange()).with("guard.getPerSectorToday");
+    }
+
+    @Bean
+    public Queue getNumGuardsPerSectQueue() {
+        return new Queue("guard.getNumPerSector");
+    }
+
+    @Bean
+    public Binding getNumGuardsPerSectBinding() {
+        return BindingBuilder.bind(getNumGuardsPerSectQueue()).to(guardExchange()).with("guard.getNumPerSector");
+    }
+
+    @Bean
+    public Queue isGuardAssignedQueue() {
+        return new Queue("guard.isAssigned");
+    }
+
+    @Bean
+    public Binding isGuardAssignedBinding() {
+        return BindingBuilder.bind(isGuardAssignedQueue()).to(guardExchange()).with("guard.isAssigned");
+    }
+
+    @Bean
+    public Queue getNumGuardsPerSectTodayQueue() {
+        return new Queue("guard.getNumPerSectorToday");
+    }
+
+    @Bean
+    public Binding getNumGuardsPerSectTodayBinding() {
+        return BindingBuilder.bind(getNumGuardsPerSectTodayQueue()).to(guardExchange()).with("guard.getNumPerSectorToday");
+    }
+
+    @Bean
+    public Queue isGuardWorkingQueue() {
+        return new Queue("guard.isWorking");
+    }
+
+    @Bean
+    public Binding isGuardWorkingBinding() {
+        return BindingBuilder.bind(isGuardWorkingQueue()).to(guardExchange()).with("guard.isWorking");
     }
 }
